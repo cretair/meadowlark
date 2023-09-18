c9a53fd1afe031ba979786fb23d6820b9330e0ea
Mr-Halatsan <office@cretair.com>
2023-09-18 10:04:04 +0000

    Dodanie formularza

--- meadowlark.js
+++ meadowlark.js
@@ -1,28 +1,67 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const multiparty = require('multiparty')
 
 const handlers = require('./lib/handlers')
+const weatherMiddlware = require('./lib/middleware/weather')
 
 const app = express()
 
 // konfiguracja silnika widoków Handlebars
 app.engine('handlebars', expressHandlebars.engine({
   defaultLayout: 'main',
+  helpers: {
+    section: function(name, options) {
+      if(!this._sections) this._sections = {}
+      this._sections[name] = options.fn(this)
+      return null
+    },
+  },
 }))
 app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'))
 
+app.use(weatherMiddlware)
+
 app.get('/', handlers.home)
 
-app.get('/about', handlers.about)
+// obsługa formularzy przesłanych przez przeglądarkę
+app.get('/newsletter-signup', handlers.newsletterSignup)
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
 
-// niestandardowa strona 404
+// obsługa formularzy typu fetch/JSON
+app.get('/newsletter', handlers.newsletter)
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
+
+// konkurs zdjęć z wakacji
+app.get('/contest/vacation-photo', handlers.vacationPhotoContest)
+app.get('/contest/vacation-photo-ajax', handlers.vacationPhotoContestAjax)
+app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+  const form = new multiparty.Form()
+  form.parse(req, (err, fields, files) => {
+    if(err) return handlers.vacationPhotoContestProcessError(req, res, err.message)
+    console.log('got fields: ', fields)
+    console.log('and files: ', files)
+    handlers.vacationPhotoContestProcess(req, res, fields, files)
+  })
+})
+app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
+  const form = new multiparty.Form()
+  form.parse(req, (err, fields, files) => {
+    if(err) return handlers.api.vacationPhotoContestError(req, res, err.message)
+    handlers.api.vacationPhotoContest(req, res, fields, files)
+  })
+})
+
 app.use(handlers.notFound)
-
-// niestandardowa strona 500
 app.use(handlers.serverError)
 
 if(require.main === module) {
