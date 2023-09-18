73b6f5369858f366d85341576eb67565d73a9765
Mr-Halatsan <office@cretair.com>
2023-09-18 09:09:52 +0000

    Przeniesiono fortunes do lib

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
 
+const fortune = require('./lib/fortune.js')
+
 const app = express()
 
 // konfiguracja silnika widoków Handlebars
@@ -9,23 +11,14 @@ app.engine('handlebars', expressHandlebars.engine({
 }))
 app.set('view engine', 'handlebars')
 
-app.use(express.static(__dirname + '/public'))
-
 const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => res.render('home'))
 
-const fortunes = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple.",
-]
-
 app.get('/about', (req, res) => {
-  const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-  res.render('about', { fortune: randomFortune })
+  res.render('about', { fortune: fortune.getFortune() })
 })
 
 // niestandardowa strona 404
@@ -41,6 +34,7 @@ app.use((err, req, res, next) => {
   res.render('500')
 })
 
-app.listen(port, () => console.log(
-  `Express został uruchomiony pod adresem http://localhost:${port}; ` +
-  `naciśnij Ctrl-C, aby zakończyć.`))
+app.listen(port, () => {
+  console.log( `Express został uruchomiony pod adresem http://localhost:${port}` +
+    '; naciśnij Ctrl-C, aby zakończyć.' )
+})
